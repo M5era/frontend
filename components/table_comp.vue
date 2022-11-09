84ac9a921a94fc0412b0c7afe9911566cafeb21f
Marc Serafin <marc.serafin@me.com>
2022-11-09 15:52:26 +0000

    Initial commit

---
+++ components/table_comp.vue
@@ -0,0 +1,18 @@
+new Vue({
+el: '#app',
+vuetify: new Vuetify(),
+data: function() {
+return {
+myItems: ["Value1", "Value2"],
+headers: [{
+text: 'Column',
+value: 'item' // Must match the key used in `myItemsTransformed`
+}]
+}
+},
+computed: {
+myItemsTransformed() {
+return this.myItems.map(item => ({ item }));
+}
+}
+});
